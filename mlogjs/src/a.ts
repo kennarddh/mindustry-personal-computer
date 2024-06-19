eecf20ec75bb66b6042df705e8a1b1b54006c620
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-06-19 17:30:02 +0700

    feat(mlogjs): Remove files

--- mlogjs/src/a.ts
+++
@@ -1,7 +0,0 @@
-// @mlog-skip
-
-export const x = { x: 1 }
-
-export const func = () => {
-	draw.image({ x: Math.PI * x.x, y: 0, image: Units.aegires, size: 100, rotation: 0 })
-}
--- mlogjs/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { x, func } from './a'
-
-draw.clear(x.x + 1, x.x + 1, x.x + 1)
-
-func()
