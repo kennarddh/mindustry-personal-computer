aee5526f5ff82f3c2f0109d55588e52fe8d5e4c9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-07-10 19:13:08 +0700

    fix: Support CRLF line ending

--- masm/src/Compiler.ts
+++ masm/src/Compiler.ts
@@ -173,7 +173,7 @@ class Compiler {
 						lineState = LineState.Done
 					}
 
-					console.debug({ line, opcode, value1, value2 })
+					// console.debug({ line, opcode, value1, value2 })
 
 					this.processLine(outputBuffer, line, opcode, value1, value2)
 
@@ -194,6 +194,11 @@ class Compiler {
 
 					inComment = true
 
+					break
+				}
+
+				// Ignore CR in CRLF
+				case '\r': {
 					break
 				}
 
