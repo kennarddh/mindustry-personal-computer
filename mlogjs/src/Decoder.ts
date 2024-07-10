30bcb842f5a8747ea8355a777bf88b7b559c1ada
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-07-10 23:25:40 +0700

    feat(mlogjs): Decoder

--- mlogjs/src/Decoder.ts
+++ mlogjs/src/Decoder.ts
@@ -4,43 +4,32 @@ const inputMemory = new Memory(inputMemoryBuilding, 64)
 const outputMemoryBuilding = getBuilding('cell2')
 const outputMemory = new Memory(outputMemoryBuilding, 64)
 
-inputMemory[0] = 0b0111111111001000010010011010110001011001001010010101000100010000
+inputMemory[0] = 3819052505518571520
+inputMemory[1] = 20
 
-const encoded = inputMemory[0]
+const encoded1 = inputMemory[0]
+const encoded2 = inputMemory[1]
 
-const opcodeMask = 0b0111111110000000000000000000000000000000000000000000000000000000
-const opcodeRightShift = 55
+const opcodeMask = 0b1111111100000000000000000000000000000000000000000000000000000000
+const opcodeRightShift = 56
 
-const value1DataTypeMask = 0b0000000001000000000000000000000000000000000000000000000000000000
-const value1DataTypeRightShift = 54
+const value1InKindMask = 0b0000000011111111000000000000000000000000000000000000000000000000
+const value1InKindRightShift = 48
 
-const value2DataTypeMask = 0b0000000000110000000000000000000000000000000000000000000000000000
-const value2DataTypeRightShift = 52
+const value1Mask = 0b0000000000000000111111111111111100000000000000000000000000000000
+const value1RightShift = 32
 
-const value1SignMask = 0b0000000000001000000000000000000000000000000000000000000000000000
-const value1SignRightShift = 51
+const value2InKindMask = 0b0000000000000000000000000000000011111111000000000000000000000000
+const value2InKindRightShift = 24
 
-const value2SignMask = 0b0000000000000100000000000000000000000000000000000000000000000000
-const value2SignRightShift = 50
-
-const value1Mask = 0b0000000000000011111111111111111111111110000000000000000000000000
-const value1RightShift = 25
-
-const value2Mask = 0b0000000000000000000000000000000000000001111111111111111111111000
-const value2RightShift = 3
-
-const opcode = (encoded & opcodeMask) >> opcodeRightShift
-const value1DataType = (encoded & value1DataTypeMask) >> value1DataTypeRightShift
-const value2DataType = (encoded & value2DataTypeMask) >> value2DataTypeRightShift
-const value1Sign = (encoded & value1SignMask) >> value1SignRightShift
-const value2Sign = (encoded & value2SignMask) >> value2SignRightShift
-const value1 = (encoded & value1Mask) >> value1RightShift
-const value2 = (encoded & value2Mask) >> value2RightShift
+const opcode = (encoded1 & opcodeMask) >> opcodeRightShift
+const value1InKind = (encoded1 & value1InKindMask) >> value1InKindRightShift
+const value1 = (encoded1 & value1Mask) >> value1RightShift
+const value2InKind = (encoded1 & value2InKindMask) >> value2InKindRightShift
+const value2 = encoded2
 
 outputMemory[0] = opcode
-outputMemory[1] = value1DataType
-outputMemory[2] = value2DataType
-outputMemory[3] = value1Sign
-outputMemory[4] = value2Sign
-outputMemory[5] = value1
-outputMemory[6] = value2
+outputMemory[1] = value1InKind
+outputMemory[2] = value1
+outputMemory[3] = value2InKind
+outputMemory[4] = value2
